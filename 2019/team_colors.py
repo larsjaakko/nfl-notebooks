33bf0ece084be016d9b9f3c8a3f46e1c74c542a0
Lars Jaakko Havro <larsjh@gmail.com>
2019-10-06 22:36:36 +0200

    Lots of update

---
+++ 2019/team_colors.py
@@ -0,0 +1,81 @@
+def get_colors():
+    
+    team_colors_main = {
+        'SF': '#AA0000',
+        'KC': '#E31837',
+        'ARI': '#97233F',
+        'ATL': '#A71930',
+        'BAL': '#241773',
+        'PHI': '#004953',
+        'NYG': '#0B2265',
+        'NO': '#9F8958',
+        'NE': '#002244',
+        'MIN': '#4F2683',
+        'MIA': '#008E97',
+        'IND': '#002C5F',
+        'CLE': '#FB4F14',
+        'LAC': '#002244',
+        'DEN': '#002244',
+        'CAR': '#0085CA',
+        'GB': '#203731',
+        'DET': '#005A8B',
+        'OAK': '#A5ACAF',
+        'CIN': '#FB4F14',
+        'WAS': '#773141',
+        'TEN': '#002244',
+        'TB': '#D50A0A',
+        'PIT': '#FFB612',
+        'NYJ': '#203731',
+        'BUF': '#00338D',
+        'LA': '#002244',
+        'JAX': '#006778',
+        'DAL': '#002244',
+        'CHI': '#0B162A',
+        'HOU': '#03202F',
+        'SEA': '#002244',
+        'SD': '#002244',
+        'JAC': '#002244'
+    }
+
+
+    return team_colors_main
+
+def get_grays():
+
+    team_colors_gray = {
+
+        'SF': '#AA0000',
+        'KC': '#D9D9D9',
+        'ARI': '#D9D9D9',
+        'ATL': '#D9D9D9',
+        'BAL': '#D9D9D9',
+        'PHI': '#D9D9D9',
+        'NYG': '#D9D9D9',
+        'NO': '#D9D9D9',
+        'NE': '#D9D9D9',
+        'MIN': '#D9D9D9',
+        'MIA': '#D9D9D9',
+        'IND': '#D9D9D9',
+        'CLE': '#D9D9D9',
+        'LAC': '#D9D9D9',
+        'DEN': '#D9D9D9',
+        'CAR': '#D9D9D9',
+        'GB': '#D9D9D9',
+        'DET': '#D9D9D9',
+        'OAK': '#D9D9D9',
+        'CIN': '#D9D9D9',
+        'WAS': '#D9D9D9',
+        'TEN': '#D9D9D9',
+        'TB': '#D9D9D9',
+        'PIT': '#D9D9D9',
+        'NYJ': '#D9D9D9',
+        'BUF': '#D9D9D9',
+        'LA': '#D9D9D9',
+        'JAX': '#D9D9D9',
+        'DAL': '#D9D9D9',
+        'CHI': '#D9D9D9',
+        'HOU': '#D9D9D9',
+        'SEA': '#D9D9D9'
+    }
+    
+    return team_colors_gray
